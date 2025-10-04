fa874bc2cdf8a458c0ac8d77c5069b49c818ace9
Jaume Barber <jbarber@hipervincle.com>
2025-10-04 14:08:33 +0200

    feat: add omz installation path

--- src/alpine-ohmyzsh/install.sh
+++ src/alpine-ohmyzsh/install.sh
@@ -20,37 +20,41 @@ if [ -z "$_CONTAINER_USER_HOME" ]; then
   fi
 fi
 
-su -c "wget -qO- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh -s" $_CONTAINER_USER
+if [ -z "$ZSHPATH" ]; then
+  ZSHPATH=$DEFAULTZSHPATH
+fi
+
+KEEP_ZSHRC=yes ZSH=$ZSHPATH su -c "wget -qO- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh -s" $_CONTAINER_USER
 
 sed -i 's/plugins=(git)/plugins=(\n)/' $_CONTAINER_USER_HOME/.zshrc
 
 if echo "$PLUGINS" | grep -w -q "zsh-autosuggestions"; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions $_CONTAINER_USER_HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-autosuggestions $_CONTAINER_USER_HOME/$ZSHPATH/custom/plugins/zsh-autosuggestions
 fi
 
 if echo "$PLUGINS" | grep -w -q "zsh-syntax-highlighting"; then
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting $_CONTAINER_USER_HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting $_CONTAINER_USER_HOME/$ZSHPATH/custom/plugins/zsh-syntax-highlighting
 fi
 
 if echo "$PLUGINS" | grep -w -q "autoupdate"; then
-  git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $_CONTAINER_USER_HOME/.oh-my-zsh/custom/plugins/autoupdate
+  git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $_CONTAINER_USER_HOME/$ZSHPATH/custom/plugins/autoupdate
 fi
 
 if echo "$PLUGINS" | grep -w -q "autojump"; then
-  git clone https://github.com/wting/autojump $_CONTAINER_USER_HOME/.oh-my-zsh/custom/plugins/autojump
+  git clone https://github.com/wting/autojump $_CONTAINER_USER_HOME/$ZSHPATH/custom/plugins/autojump
   apk --no-cache add python3
-  su -c "cd $_CONTAINER_USER_HOME/.oh-my-zsh/custom/plugins/autojump/ && SHELL=zsh && ./install.py" $_CONTAINER_USER
+  su -c "cd $_CONTAINER_USER_HOME/$ZSHPATH/custom/plugins/autojump/ && SHELL=zsh && ./install.py" $_CONTAINER_USER
   echo $'\n[[ -s ~/.autojump/etc/profile.d/autojump.sh ]] && source ~/.autojump/etc/profile.d/autojump.sh' >> $_CONTAINER_USER_HOME/.zshrc
   echo $'\nautoload -U compinit && compinit -u' >> $_CONTAINER_USER_HOME/.zshrc
 fi
 
 if echo "$PLUGINS" | grep -w -q "alias-tips"; then
   apk --no-cache add python3
-  git clone https://github.com/djui/alias-tips $_CONTAINER_USER_HOME/.oh-my-zsh/custom/plugins/alias-tips
+  git clone https://github.com/djui/alias-tips $_CONTAINER_USER_HOME/$ZSHPATH/custom/plugins/alias-tips
 fi
 
 if echo "$PLUGINS" | grep -w -q "powerlevel10k"; then
-  git clone https://github.com/romkatv/powerlevel10k.git $_CONTAINER_USER_HOME/.oh-my-zsh/custom/themes/powerlevel10k
+  git clone https://github.com/romkatv/powerlevel10k.git $_CONTAINER_USER_HOME/$ZSHPATH/custom/themes/powerlevel10k
   curl -L "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf" -o /usr/share/fonts/MesloLGS\ NF\ Regular.ttf
   curl -L "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf" -o /usr/share/fonts/MesloLGS\ NF\ Bold.ttf
   curl -L "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf" -o /usr/share/fonts/MesloLGS\ NF\ Italic.ttf
